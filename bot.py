5600e75862de2d600f9fa233d792d0a569aefac0
Zhenya_01 <zhenya170401@gmail.com>
2022-11-26 23:38:24 +0500

    initial commit

--- bot.py
+++ bot.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
+import os
+import sys
+import traceback
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import regs
+from telegram.ext import Updater, CallbackContext, CommandHandler, \
+    MessageHandler, Filters
+from telegram import Update
+import logging
+
+updater = Updater(token=regs.bot_token, use_context=True)
+dispatcher = updater.dispatcher
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s: %(levelname)s, %(threadName)s, %(filename)s, %(funcName)s, %(message)s',
+    handlers=[
+        logging.FileHandler(f'{os.path.abspath(os.path.dirname(__file__))}/locker_post_bot.log'),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def logging_uncaught_exceptions(ex_type, ex_value, trace_back):
+    if issubclass(ex_type, KeyboardInterrupt):
+        sys.__excepthook__(ex_type, ex_value, trace_back)
+        return
+    logging.critical(''.join(traceback.format_tb(trace_back)))
+    logging.critical('{0}: {1}'.format(ex_type, ex_value))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+sys.excepthook = logging_uncaught_exceptions
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def start(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Привет. Я Жижов. Добро пожаловать.")
+
+
+def echo(update: Update, context: CallbackContext):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+
+
+dispatcher.add_handler(CommandHandler('start', start))
+dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
+updater.start_polling()
