55fc071099d0302eb0d1d7c94c07ebaf63bc7dc5
Zhenya_01 <zhenya170401@gmail.com>
2022-12-04 19:24:32 +0500

    echo

--- bot.py
+++ bot.py
@@ -142,8 +142,10 @@ dispatcher.add_handler(CommandHandler('loud', loud))
 # dispatcher.add_handler(CommandHandler('post', post_hello_message))
 dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
 updater.start_polling()
+logger.debug('STARTING TO SUBSCRIBE TO STREAM ONLINE')
 twitchAPI_integration.webhook.listen_stream_online(regs.ezhov_broadcaster_id,
                              callback=post_stream_live_notification)
+logger.debug('STARTING TO SUBSCRIBE TO STREAM OFFLINE')
 twitchAPI_integration.webhook.listen_stream_offline(regs.ezhov_broadcaster_id,
                              callback=post_stream_offline_notification)
 # twitchAPI_integration.webhook.listen_channel_subscribe(regs.ezhov_broadcaster_id, post_stream_notification)
