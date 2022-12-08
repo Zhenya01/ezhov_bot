332e1a168898144130c1d054dff2662a87fdfb96
Zhenya_01 <zhenya170401@gmail.com>
2022-12-08 16:23:20 +0500

    Adding test bot_file=

--- bot_test.py
+++ bot_test.py
@@ -3,15 +3,18 @@ import os
 import sys
 import traceback
 import random
+import datetime
 
+import pytz
 from telethon.errors import ChatNotModifiedError
 from telethon.sync import TelegramClient
 from telethon import functions
 
 import regs
 from telegram.ext import Updater, CallbackContext, CommandHandler, \
-    MessageHandler, Filters, PicklePersistence, Dispatcher, ExtBot, JobQueue
-from telegram import Update, Bot
+    MessageHandler, Filters, PicklePersistence, Dispatcher, ExtBot, JobQueue, \
+    Defaults
+from telegram import Update, Bot, ChatPermissions
 import twitchAPI_integration
 import logging
 import asyncio
@@ -63,11 +66,15 @@ def echo(update: Update, context: CallbackContext):
     if update.effective_chat.type == 'private':
         context.bot.send_message(chat_id=update.effective_chat.id, text=f'Все говорят: "{update.message.text}", а ты возьми, да и купи слона!')
 
-
 async def post_stream_live_notification_zhenya(data):
     if 'silent' not in updater.dispatcher.bot_data.keys() or updater.dispatcher.bot_data['silent'] is False:
         emoji = random.choice(regs.emoji_list)
-        phrase = random.choice(updater.dispatcher.bot_data['phrases_list'])
+        phrases_list = updater.dispatcher.bot_data['phrases_list']
+        if len(phrases_list) == 0:
+            phrase = 'Присоединяйтесь к моей трансляции Twitch!'
+        else:
+            phrase = updater.dispatcher.bot_data['phrases_list'][0]
+            del updater.dispatcher.bot_data['phrases_list'][0]
         notification_text = f'{emoji} {phrase}'
         notification_text += '\ntwitch.tv/zdarovezhov'
         updater.dispatcher.bot.send_message(-1001879046742, notification_text)
@@ -75,17 +82,26 @@ async def post_stream_live_notification_zhenya(data):
         updater.dispatcher.bot_data['silent'] = True
     await rename_channel_zhenya(live=True)
 
-
 async def post_stream_offline_notification_zhenya(data):
     await rename_channel_zhenya(live=False)
 
 
 def mute(update: Update, context: CallbackContext):
-    admins_list = regs.admins_list + [1255841663, 1149122366, 1238357873]
-    if update.effective_user.id in admins_list:
-        duration = context.args[0]
+    admins_list = regs.admins_list + regs.moders_list
+    print('STARTING MUTE. CHECKING ADMINS')
+    if update.message.from_user.id in admins_list:
+        print('IS_ADMIN. MUTING')
+        if len(context.args) == 0:
+            duration = 30
+        else:
+            duration = int(context.args[0])
         mute_id = update.message.reply_to_message.from_user.id
-        context.bot.restrict_chat_member()
+        until_date = datetime.datetime.now() + datetime.timedelta(minutes=duration)
+        permissions = ChatPermissions(can_send_messages=False)
+        context.bot.restrict_chat_member(regs.zhenya_group_id, mute_id,
+                                         permissions, until_date)
+    else:
+        print('NOT_ADMIN. SKIPPING')
 
 
 async def rename_channel_zhenya(live: bool):
@@ -123,7 +139,8 @@ class EzhovUpdater(Updater):
     def __init__(self, token):
         # con_pool_size = 4 + 4
         # request_kwargs = {"con_pool_size": con_pool_size}
-        bot = Bot(token)
+        bot = Bot(token,
+                  defaults=Defaults(tzinfo=pytz.timezone('Europe/Moscow')))
         persistence = PicklePersistence(
             filename=f'{os.path.abspath(os.path.dirname(__file__))}/bot_persistence_zhenya')
 
@@ -163,7 +180,7 @@ dispatcher.add_handler(CommandHandler('mute', mute, Filters.reply))
 # dispatcher.add_handler(CommandHandler('post', post_hello_message))
 dispatcher.add_handler(MessageHandler(Filters.text & (~Filters.command), echo))
 updater.start_polling()
-asyncio.run(main())
+# asyncio.run(main())
 logger.debug('STARTING TO SUBSCRIBE TO STREAM ONLINE')
 # subscribe_stream_online()
 logger.debug('STARTING TO SUBSCRIBE TO STREAM OFFLINE')
