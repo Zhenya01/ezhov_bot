9a0bc77ed4d3fa3d9b1341911bc58796e0ec6d92
Zhenya_01 <zhenya170401@gmail.com>
2023-01-18 05:00:12 +0500

    v20 transition

--- bot.py
+++ bot.py
@@ -98,7 +98,7 @@ async def post_stream_live_notification(data):
             del application.bot_data['phrases_list'][0]
         notification_text = f'{emoji} {phrase}'
         notification_text += '\ntwitch.tv/zdarovezhov'
-        await application.bot.send_message(regs.zhenya_channel_id, notification_text)
+        await application.bot.send_message(regs.zdarovezhov_channel_id, notification_text)
     else:
         application.bot_data['silent'] = False
     await rename_channel(live=True)
