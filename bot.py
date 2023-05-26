5ac1fdb77491c851d9ae946623d7ecfc17295ace
Zhenya <zhenya170401@gmail.com>
2023-05-27 03:27:20 +0800

    Testing and updating tiktoks event channel id

--- bot.py
+++ bot.py
@@ -150,8 +150,8 @@ application.add_handler(
                    chat_management_module.kick_from_group))
 application.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_TITLE,
                                        twitch_module.schedule_remove_rename_message))
-# application.add_handler(MessageHandler(filters.ATTACHMENT,
-#                                        tiktok_module.get_ticktock_file))
+application.add_handler(MessageHandler(filters.VIDEO,
+                                       tiktok_module.waiting_for_ticktock))
 # application.add_handler(CommandHandler('file', tiktok_module.get_ticktock_file))
 # application.add_handler(CommandHandler('post', post_hello_message))
 application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), echo))
