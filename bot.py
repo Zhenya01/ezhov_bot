fc071986c2b58432523299f14b2e62511a16a753
Zhenya <zhenya170401@gmail.com>
2023-05-27 13:51:30 +0800

    Testing and updating tiktoks event channel id

--- bot.py
+++ bot.py
@@ -88,9 +88,9 @@ async def functions_to_run_at_the_beginning(_):
 
 conv_handler = ConversationHandler(
         entry_points=[
-                      # CommandHandler("send_tiktok", tiktok_module.waiting_for_tiktok, filters=filters.ChatType.PRIVATE),
-                      # CommandHandler("start", tiktok_module.waiting_for_tiktok,
-                      #                filters=filters.Regex(rf'{SEND_TIKTOK_DEEPLINK}') & filters.ChatType.PRIVATE),
+                      CommandHandler("send_tiktok", tiktok_module.waiting_for_tiktok, filters=filters.ChatType.PRIVATE),
+                      CommandHandler("start", tiktok_module.waiting_for_tiktok,
+                                     filters=filters.Regex(rf'{SEND_TIKTOK_DEEPLINK}') & filters.ChatType.PRIVATE),
                       CommandHandler('start_approval', tiktok_module.show_tiktok_to_approve)],
         states={
             # WAITING_FOR_TIKTOK:
