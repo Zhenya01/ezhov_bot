cf636f34e9931b7de4b80eee7fdd2687a440dbb6
Zhenya <zhenya170401@gmail.com>
2023-05-28 17:59:32 +0800

    Forwarding

--- forward_posts.py
+++ forward_posts.py
@@ -8,4 +8,5 @@ async def forward_post(update:Update, context: ContextTypes.DEFAULT_TYPE):
     thread_id = update.message.message_thread_id
     if thread_id == regs.ezhov_forum_threads['posts']:
         context.bot.forward_message(update.message.chat_id,
+                                    update.message.chat_id,
                                     update.message.message_id)
