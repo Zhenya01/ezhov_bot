b559103b17ecdd086a52406319f5311938577f34
Zhenya <zhenya170401@gmail.com>
2023-05-28 18:45:51 +0800

    Forwarding

--- forward_posts.py
+++ forward_posts.py
@@ -6,10 +6,10 @@ from helpers_module import logger
 
 
 async def forward_post(update:Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.debug(f'forward_post_triggered')
     thread_id = update.message.message_thread_id
     logger.debug(f'Thread_id - {thread_id}, Posts_thread_id - {regs.ezhov_forum_threads["posts"]}')
     if thread_id == regs.ezhov_forum_threads['posts']:
+        logger.debug(f'forward_post_triggered')
         context.bot.forward_message(update.message.chat_id,
                                     update.message.chat_id,
                                     update.message.message_id,
