a90396e10e4261df54a5931a62095a63a5e15e9b
Zhenya <zhenya170401@gmail.com>
2023-08-24 23:53:46 +0500

    Infinite group call

--- group_calls_module.py
+++ group_calls_module.py
@@ -6,11 +6,12 @@ import regs
 
 app = TelegramClient('EzhovAudio', regs.ezhov_audio_api_id, regs.ezhov_audio_api_hash).start()
 group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON)
-group_call = group_call_factory.get_file_group_call('input.raw')
+group_call = group_call_factory.get_raw_group_call()
+    # get_file_group_call('input.raw')
 
 
 @app.on(events.NewMessage(pattern='Hello'))
 async def join_handler(event):
-    await group_call.start(regs.zhenya_forum_id)
+    await group_call.start(regs.secrets_group_id)
 
 app.run_until_disconnected()
