e8b8df6e68220b1b3d3e9024185088af5d000ef6
Zhenya <zhenya170401@gmail.com>
2023-08-26 21:54:12 +0500

    group_calls_fix

--- group_calls_module.py
+++ group_calls_module.py
@@ -4,13 +4,13 @@ from pytgcalls import GroupCallFactory
 
 import regs
 
-app = TelegramClient('EzhovAudio', regs.ezhov_audio_api_id, regs.ezhov_audio_api_hash).start()
+app = TelegramClient('EzhovCalls', regs.ezhov_audio_api_id, regs.ezhov_audio_api_hash).start()
 group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON)
 group_call = group_call_factory.get_raw_group_call()
     # get_file_group_call('input.raw')
 
 
-@app.on(events.NewMessage(pattern='Hello'))
+@app.on(events.NewMessage(pattern='Начинай'))
 async def join_handler(event):
     await group_call.start(regs.secrets_group_id)
 
