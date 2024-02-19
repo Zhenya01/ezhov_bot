634a1e74c0a106c0c5d8fb9580f55e1f28df9d12
Zhenya <zhenya170401@gmail.com>
2024-02-19 22:25:41 +0500

    fixes

--- group_calls_module.py
+++ group_calls_module.py
@@ -5,11 +5,8 @@ from telethon import TelegramClient, events
 from pytgcalls import GroupCallFactory
 
 import cfg
-pprint.pp(cfg.config_data)
-print(cfg.config_data['EZHOV_AUDIO_API_ID'])
-print(cfg.config_data['EZHOV_AUDIO_API_HASH'])
 # app = TelegramClient('EzhovCalls', regs.ezhov_audio_api_id, regs.ezhov_audio_api_hash).start()
-app = TelegramClient('EzhovCalls', cfg.config_data['EZHOV_AUDIO_API_ID'], cfg.config_data['EZHOV_AUDIO_API_HASH']).start()
+app = TelegramClient('EzhovCalls', cfg.config_data['KEYS']['EZHOV_AUDIO_API_ID'], cfg.config_data['KEYS']['EZHOV_AUDIO_API_HASH']).start()
 
 group_call_factory = GroupCallFactory(app, GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON)
 group_call = group_call_factory.get_raw_group_call()
